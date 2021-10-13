87d114da6518367769d4b37aeca14af469b9b4bd
Yankai Li <yl4632@columbia.edu>
2021-10-12 22:08:06 -0400

    Update lambda_function.py

--- lambda-functions/LF0/lambda_function.py
+++ lambda-functions/LF0/lambda_function.py
@@ -5,8 +5,7 @@ import uuid
 # use access key and secret key to achieve cross-account connection to Lex Bot
 # Cannot provide this information in the submission
 client = boto3.client('lex-runtime', region_name='us-west-2',aws_access_key_id = '********', aws_secret_access_key = '***************')
-# client = boto3.client('lex-runtime')
-last_response = None
+
 
 def lambda_handler(event, context):
     
