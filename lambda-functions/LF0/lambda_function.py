c01dba7b94b37b705029d11471ce478a331c3d22
Yankai Li <yl4632@columbia.edu>
2021-10-12 22:07:37 -0400

    Update lambda_function.py

--- lambda-functions/LF0/lambda_function.py
+++ lambda-functions/LF0/lambda_function.py
@@ -23,20 +23,6 @@ def lambda_handler(event, context):
     )
     last_response = response
     
-    # now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-
-    # msg = {"id": event['id'], "text": response['message'], "timestamp": now}
-    
-    # return {
-    #     'statusCode': 200,
-    #     'headers': {
-    #         'Access-Control-Allow-Headers': 'Content-Type',
-    #         'Access-Control-Allow-Origin': '*',
-    #         'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
-    #     },
-    #     'body': json.dumps(event['messages'][0]['unstructured']['text'])
-    # }
-    
     return {
       "messages": [
         {
